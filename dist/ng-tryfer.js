2fdff2fd1ab9901dd3b7bb6e4c0cc7156e4936d9
zhaoxuan <zhaoxuan1727@gmail.com>
2014-08-11 15:06:28 +0800

    init

---
+++ dist/ng-tryfer.js
@@ -0,0 +1,19 @@
+/**
+ * Restful Resources service for AngularJS apps
+ * @version v1.4.0 - 2014-04-25 * @link https://github.com/mgonto/restangular
+ * @author Martin Gontovnikas <martin@gon.to>
+ * @license MIT License, http://www.opensource.org/licenses/MIT
+ */
+'use strict';
+(function() {
+
+  var module = angular.module('ng-tryfer', []);
+
+  module.provider('ng-tryfer', function() {
+
+  });
+
+
+
+
+})();
